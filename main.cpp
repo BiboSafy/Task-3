7aa4116a09c97a02472d134dad9f14eb550c926e
BiboSafy <es-habbiba.attia2027@alexu.edu.eg>
2025-08-01 11:45:28 -0700

    commit No2

---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int subjects;
+    char Grade ;
+    int hours ;
+
+    cout << "Enter number of subjects";
+    cin >> subjects;
+
+    for (int i = 0 , i < subjects , i ++)
+    {
+        cout << "Enter subject";
+        c
+    }
+    return 0;
+}
